9ed6f1e33a831d24fb8ca99c977456a44504ae82
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-24 14:34:58 +0200

    added translations

--- de_skycoder42_qtmvvm_settings_core.pri
+++ de_skycoder42_qtmvvm_settings_core.pri
@@ -7,7 +7,10 @@ SOURCES += \
 	$$PWD/settingscontrol.cpp \
 	$$PWD/xmlsettingssetuploader.cpp
 
-INCLUDEPATH += $$PWD
-
 DISTFILES += \
     $$PWD/lupdate_xml.py
+
+QPM_TRANSLATIONS += $$PWD/qtmvvm_settings_core_de.ts \
+	$$PWD/qtmvvm_settings_core_template.ts
+
+INCLUDEPATH += $$PWD
---
+++ qtmvvm_settings_core_de.ts
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>SettingsControl</name>
+    <message>
+        <location filename="settingscontrol.cpp" line="131"/>
+        <source>Restore Defaults?</source>
+        <translation>Standards wiederherstellen?</translation>
+    </message>
+    <message>
+        <location filename="settingscontrol.cpp" line="132"/>
+        <source>All custom changes will be deleted and the defaults restored. &lt;i&gt;This cannot be undone!&lt;/i&gt;</source>
+        <translation>Alle Änderungen werden gelöscht und die Standardwerte wiederhergestellt. &lt;i&gt;Dies kann nicht rückgängig gemacht werden!&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="settingscontrol.cpp" line="133"/>
+        <source>Yes</source>
+        <translation>Ja</translation>
+    </message>
+    <message>
+        <location filename="settingscontrol.cpp" line="134"/>
+        <source>No</source>
+        <translation>Nein</translation>
+    </message>
+</context>
+<context>
+    <name>XmlSettingsSetupLoader</name>
+    <message>
+        <location filename="xmlsettingssetuploader.cpp" line="280"/>
+        <source>General Settings</source>
+        <translation>Allgemeine Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="xmlsettingssetuploader.cpp" line="288"/>
+        <source>General</source>
+        <translation>Allgemein</translation>
+    </message>
+</context>
+</TS>
