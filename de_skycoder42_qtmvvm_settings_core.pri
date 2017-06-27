2705a637b6aeb2f4e57e189a629553109be6081a
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-06-27 11:40:54 +0200

    automated settings-ts lupdate

--- de_skycoder42_qtmvvm_settings_core.pri
+++ de_skycoder42_qtmvvm_settings_core.pri
@@ -8,9 +8,17 @@ SOURCES += \
 	$$PWD/xmlsettingssetuploader.cpp
 
 DISTFILES += \
-    $$PWD/lupdate_xml.py
+	$$PWD/lupdate_xml.py
 
 QPM_TRANSLATIONS += $$PWD/qtmvvm_settings_core_de.ts \
 	$$PWD/qtmvvm_settings_core_template.ts
 
 INCLUDEPATH += $$PWD
+
+!isEmpty(QTMVVM_SETTINGS_FILE) {
+	settingsts.target = lupdate-settings
+	settingsts.commands = $$shell_quote($$shell_path($$PWD/lupdate_xml.py)) $$QTMVVM_SETTINGS_TS $$QTMVVM_SETTINGS_LOCALES
+	lupdate.depends += lupdate-settings
+
+	QMAKE_EXTRA_TARGETS += lupdate settingsts
+}
--- lupdate_xml.py
+++ lupdate_xml.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
-# Usage: lupdate_xml.py xml_source ts_file [locale]
-# Example: lupdate_xml.py settings.xml settings_de.ts de_DE
+# Usage: lupdate_xml.py xml_source locales
+# Example: lupdate_xml.py settings.xml de template
 
 import sys
 from xml.etree.ElementTree import ElementTree, Element, SubElement, parse, tostring
 from xml.dom import minidom
 
 src = sys.argv[1]
-dest = sys.argv[2]
-locale = ""
-if len(sys.argv) > 3:
-	locale = sys.argv[3]
+locales = sys.argv[2:]
 
 trstrings = set()
 
@@ -24,22 +21,24 @@ for elem in tree.iter():
 		if "tooltip" in elem.attrib:
 			trstrings.add(elem.attrib["tooltip"])
 
-root = Element("TS")
-root.attrib["version"] = "2.1"
-root.attrib["language"] = locale
+	root = Element("TS")
+	root.attrib["version"] = "2.1"
 
-context = SubElement(root, "context")
-name = SubElement(context, "name")
-name.text = "qtmvvm_settings_xml"
+	context = SubElement(root, "context")
+	name = SubElement(context, "name")
+	name.text = "qtmvvm_settings_xml"
 
-for str in trstrings:
-	message = SubElement(context, "message")
-	source = SubElement(message, "source")
-	source.text = str
-	translation = SubElement(message, "translation")
-	translation.attrib["type"] = "unfinished"
+	for str in trstrings:
+		message = SubElement(context, "message")
+		source = SubElement(message, "source")
+		source.text = str
+		translation = SubElement(message, "translation")
+		translation.attrib["type"] = "unfinished"
 
-outFile = open(dest, "w")
-tree = ElementTree(root)
-tree.write(outFile, encoding="unicode", xml_declaration=True, short_empty_elements=False)
-outFile.close()
+for locale in locales:
+	root.attrib["language"] = locale
+
+	outFile = open("qtmvvm_settings_xml" + locale + ".ts", "w")
+	tree = ElementTree(root)
+	tree.write(outFile, encoding="unicode", xml_declaration=True, short_empty_elements=False)
+	outFile.close()
