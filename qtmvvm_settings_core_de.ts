40cdcbdc8bfe92ee6fae1c9c26f00aaa23f88220
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-10-15 00:40:08 +0200

    ts update

--- qtmvvm_settings_core_de.ts
+++ qtmvvm_settings_core_de.ts
@@ -4,22 +4,22 @@
 <context>
     <name>SettingsControl</name>
     <message>
-        <location filename="settingscontrol.cpp" line="131"/>
+        <location filename="settingscontrol.cpp" line="+131"/>
         <source>Restore Defaults?</source>
         <translation>Standards wiederherstellen?</translation>
     </message>
     <message>
-        <location filename="settingscontrol.cpp" line="132"/>
+        <location line="+1"/>
         <source>All custom changes will be deleted and the defaults restored. &lt;i&gt;This cannot be undone!&lt;/i&gt;</source>
         <translation>Alle Änderungen werden gelöscht und die Standardwerte wiederhergestellt. &lt;i&gt;Dies kann nicht rückgängig gemacht werden!&lt;/i&gt;</translation>
     </message>
     <message>
-        <location filename="settingscontrol.cpp" line="133"/>
+        <location line="+1"/>
         <source>Yes</source>
         <translation>Ja</translation>
     </message>
     <message>
-        <location filename="settingscontrol.cpp" line="134"/>
+        <location line="+1"/>
         <source>No</source>
         <translation>Nein</translation>
     </message>
@@ -27,12 +27,12 @@
 <context>
     <name>XmlSettingsSetupLoader</name>
     <message>
-        <location filename="xmlsettingssetuploader.cpp" line="280"/>
+        <location filename="xmlsettingssetuploader.cpp" line="+280"/>
         <source>General Settings</source>
         <translation>Allgemeine Einstellungen</translation>
     </message>
     <message>
-        <location filename="xmlsettingssetuploader.cpp" line="288"/>
+        <location line="+8"/>
         <source>General</source>
         <translation>Allgemein</translation>
     </message>
